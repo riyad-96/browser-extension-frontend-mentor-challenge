c15e5a6d6cd94afe75770a43a325bae0feafc631
Riyad <onlygobs0@gmail.com>
2025-05-03 13:41:09 +0600

    --

--- app.js
+++ app.js
@@ -257,7 +257,7 @@ function sunIcon() {
       d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z" />
   </g>
   <defs>
-    <clipPath id="a">
+    <clipPath>
       <path fill="#fff" d="M0 0h22v22H0z" />
     </clipPath>
   </defs>
@@ -270,7 +270,7 @@ function moonIcon() {
       d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z" />
   </g>
   <defs>
-    <clipPath id="a">
+    <clipPath>
       <path fill="#fff" d="M0 0h22v22H0z" />
     </clipPath>
   </defs>
